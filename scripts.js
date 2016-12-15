fcac6c9ba0454c2e30cfacb1aa68682c37ec8b53
andrewplan <a.plan88@gmail.com>
2016-12-15 13:29:19 -0600

    Outlined code for the functions I need for the features requested.

---
+++ scripts.js
@@ -0,0 +1,32 @@
+$( document ).ready( () => {
+    console.log( 'Hello World!' );
+
+    function runCounter( num ) {
+        // count up incrementally every second
+        // stop once the given number is reached
+    }
+
+    function addHighlight( wordType ) {
+        // check wordType if "fingers" or "toes"
+        // highlight word
+    }
+
+    function removeHighlight( wordType ) {
+        // check wordType if "fingers" or "toes"
+        // remove highlight from word
+    }
+
+    function resetCounter() {
+        // reset counter to 0
+        // remove highlights from both words
+        // clear input box
+    }
+
+    function restartCounter() {
+        // run resetCounter function
+        // run runCounter function with the original user input
+        // if no user input, run runCounter starting at 0
+    }
+
+
+} );
