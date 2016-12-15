2984b2a30764dea0c05416aabb7ae328d109d8e6
andrewplan <a.plan88@gmail.com>
2016-12-15 15:13:38 -0600

    Finished coding counter and highlight/un-highlight functions, need to do reset and restart functions next.  Also moved all code to inside of document.ready block.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,72 @@
 $( document ).ready( () => {
-    console.log( 'Hello World!' );
+    let numInput = $( '.number-input-form-number-input-box' );
+    let startCounter = $( '.start-counter' );
+    let resetCounter = $( '.reset-counter' );
+    let restartCounter = $( '.restart-counter' );
+    let fingersWordWrapper = $( '.fingers-and-toes-container-fingers-word-wrapper' );
+    let toesWordWrapper = $( '.fingers-and-toes-container-toes-word-wrapper' );
+    let counterText = $( '.counter-wrapper-counter-text' );
 
-    function runCounter( num ) {
+    startCounter.click( () => {
+        event.preventDefault();
+        let numVal = $( '.number-input-form-number-input-box' ).val();
         // count up incrementally every second
         // stop once the given number is reached
+        for ( let i = 0; i <= numVal; i++ ) {
+            setTimeout( incrementCounter( i ), i * 1000 );
+        }
+
+        function incrementCounter( savedReference ) {
+            return function() {
+                if ( savedReference > 0 ) {
+                    if ( savedReference % 3 === 0 && savedReference % 5 === 0 ) {
+                        addHighlight( fingersWordWrapper );
+                        addHighlight( toesWordWrapper );
+                    }
+                    else if ( savedReference % 3 === 0 ) {
+                        addHighlight( fingersWordWrapper );
+                        removeHighlight( toesWordWrapper );
+                    }
+                    else if ( savedReference % 5 === 0 ) {
+                        addHighlight( toesWordWrapper );
+                        removeHighlight( fingersWordWrapper );
+                    }
+                    else {
+                        removeHighlight( fingersWordWrapper );
+                        removeHighlight( toesWordWrapper );
+                    }
+                }
+                counterText.text( savedReference );
+            }
+        }
+
+    } )
+
+    function addHighlight( wrapper ) {
+        // add highlight to word
+        wrapper.addClass( 'highlighted' );
     }
 
-    function addHighlight( wordType ) {
-        // check wordType if "fingers" or "toes"
-        // highlight word
-    }
-
-    function removeHighlight( wordType ) {
-        // check wordType if "fingers" or "toes"
+    function removeHighlight( wrapper ) {
         // remove highlight from word
+        wrapper.removeClass( 'highlighted' );
     }
 
-    function resetCounter() {
+    resetCounter.click( () => {
         // reset counter to 0
+        counterText.val( 0 );
         // remove highlights from both words
+        fingersWordWrapper.removeClass( 'highlighted' );
+        toesWordWrapper.removeClass( 'highlighted' );
         // clear input box
-    }
+        numInput.val( '' );
+    } );
 
-    function restartCounter() {
+    restartCounter.click( () => {
         // run resetCounter function
         // run runCounter function with the original user input
         // if no user input, run runCounter starting at 0
-    }
+    } );
 
 
 } );
